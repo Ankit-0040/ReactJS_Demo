dd9b86da51942f3f02d3bfcffdfe96d77e64d89e
Ankit-0040 <ankitssingh1912@gmail.com>
2023-05-11 08:53:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+      <Navbar title="Textutils"/>
+      </>
+    
+    </div>
+  );
+}
+
+export default App;
